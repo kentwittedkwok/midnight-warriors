b940e3c6907608a4fd9b8e2632a8703fe6fc575a
kentwittedkwok <kwok.kent@gmail.com>
2016-08-25 11:47:13 +1000

    Add files via upload

---
+++ CountDown.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class  CountDown {
+  public static void count_down (int n) {
+    if (n <= 0) 				// don’t use ==, why?
+      System.out.println ("BLAST OFF!!!!"); 	    
+    else {
+      System.out.println("Count down at time "+ n );
+	 count_down (n-1);
+    }
+  }
+
+  public static void main (String[] args) {
+    int i = 10;
+    count_down (i);
+  }
+}
+
